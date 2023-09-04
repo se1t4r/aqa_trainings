3832e20592f3dc72e156dd17e2f4fb7cf07f4e9d
se1t4r <katarsis994@gmail.com>
2023-09-04 19:23:17 +0300

    adding error popup login page

--- framework/tests/ui/test_login_selenium.py
+++ framework/tests/ui/test_login_selenium.py
@@ -1,8 +1,14 @@
 from providers.data.users_provider import UserProvider
 
 def test_check_login(github_ui_client):
-    user = UserProvider.fake_user()
-    
-    github_ui_client.login(user['login'], user['password'])
+    fake_user = UserProvider.fake_user()
+    true_user = UserProvider.existing_user()
 
+    github_ui_client.login(fake_user['login'], fake_user['password'])
+    github_ui_client.login_alert_popup()
     assert github_ui_client.get_title() == "Sign in to GitHub · GitHub"
+
+    github_ui_client.login(true_user['login'], true_user['password'])
+    github_ui_client.login_alert_popup()
+    assert github_ui_client.get_title() == "GitHub"
+    github_ui_client.logout()
